2e9c34828b771daefb32018776278e3f11934252
RickLustri <rkzinnine@gmail.com>
2024-10-30 12:09:46 -0300

    adicionando a função de logar/deslogar

--- src/routes/loginRoute.js
+++ src/routes/loginRoute.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/login', loginController.exibirLogin);
 router.post('/autenticar', loginController.logarUsuario);
+router.get('/deslogar', loginController.deslogarUsuario);
 
 
 module.exports = router
