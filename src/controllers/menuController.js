2d4f04653b173bfd47d96f8a6745feae61fcdbd3
RickLustri <rkzinnine@gmail.com>
2024-11-16 20:52:34 -0300

    add listagem do menu, e add prato na pagina de adm

--- src/controllers/menuController.js
+++ src/controllers/menuController.js
@@ -1,5 +1,5 @@
 const menuModels = require('../models/menuModels')
-const pratos = [
+const pratinhos = [
   {
     id: 1,
     nome: 'Hambúrguer',
@@ -36,13 +36,39 @@ const pratos = [
     avaliacao: []
   }
 ]
-function exibirMenu(request, response) {
-  response.render('menu', { pratos })
+async function exibirMenu(request, response) {
+  const pratos = await menuModels.listarPratos();
+
+  console.log('bebidas:', pratos.bebidas)
+  console.log('comidaJaponesa:', pratos.comidaJaponesa)
+  console.log('entrada:', pratos.entrada)
+  console.log('hamburguer:', pratos.hamburguer)
+  console.log('risoto:', pratos.risoto)
+  console.log('salada:', pratos.salada)
+  console.log('sobremesa:', pratos.sobremesa)
+  console.log('variedades:', pratos.variedades)
+
+  const logado = request.session.user ? true : false
+  const user = request.session.user
+    response.render('menu', {
+      bebidas: pratos.bebidas,
+      comidaJaponesa: pratos.comidaJaponesa,
+      entrada: pratos.entrada,
+      hamburguer: pratos.hamburguer,
+      risoto: pratos.risoto,
+      salada: pratos.salada,
+      sobremesa: pratos.sobremesa,
+      variedades: pratos.variedades,
+      pratinhos,
+      logado, user
+});
 }
 
-function adicionarPrato(request, response) {
-  const { nome, preco } = request.body
-  console.log("entrei na função de add prato")
+async function adicionarPrato(request, response) {
+  const { nome, descricao, preco, categoria } = request.body
+  const imagem = request.file.filename
+
+  await menuModels.criarPrato(nome, descricao, preco, categoria, imagem)
   
   response.redirect('/administracao')
 }
--- src/models/menuModels.js
+++ src/models/menuModels.js
@@ -1,30 +1,38 @@
-// const db = require('../config/db');
+const {PrismaClient} = require('@prisma/client');
+const prisma = new PrismaClient();
 
-// async function adicionarMenu(nome, preco) {
-     
-//     // Adicionando o novo menu
-//     await db.query(`
-//         INSERT INTO menu (nome, preco, criadoEm) 
-//         VALUES ('${nome}', '${preco}', now())
-//     `)
-//     .then(() => {
-//         console.log('Menu criado com sucesso!')
-//     })
-//     .catch((erro) => {
-//         console.error('Erro ao inserir dados, ', erro)
-//     })
-// }
 
-// async function listarMenu() {
+async function criarPrato(nome, descricao, preco, categoria, imagem) {
+  console.log('Entrou na função de add o prato!')
 
-//     const menu = await db.query(`
-//         SELECT * FROM menu
-//     `)
+  const pratos = await prisma.prato.create({
+    data:{
+      nome: nome,
+      descricao: descricao,
+      preco: preco,
+      tipo_comida: categoria,
+      imagem: imagem
+    }
+  })
+  console.log ("Prato add:", pratos) 
+  return pratos
+}
 
-//     return menu
-// }
+async function listarPratos() {
+  const [bebidas, comidaJaponesa, entrada, hamburguer, risoto, salada, sobremesa, variedades] = await Promise.all([
+    prisma.prato.findMany({ where: { tipo_comida: 'bebida' } }),
+    prisma.prato.findMany({ where: { tipo_comida: 'comida_japonesa' } }),
+    prisma.prato.findMany({ where: { tipo_comida: 'entrada' } }),
+    prisma.prato.findMany({ where: { tipo_comida: 'hamburguer' } }),
+    prisma.prato.findMany({ where: { tipo_comida: 'risoto' } }),
+    prisma.prato.findMany({ where: { tipo_comida: 'salada' } }),
+    prisma.prato.findMany({ where: { tipo_comida: 'sobremesa' } }),
+    prisma.prato.findMany({ where: { tipo_comida: 'variedade' } })
+  ]);
 
-// module.exports = {
-//     adicionarMenu,
-//     listarMenu
-// }
+  return { bebidas, comidaJaponesa, entrada, hamburguer, risoto, salada, sobremesa, variedades };
+}
+
+
+
+module.exports = { criarPrato, listarPratos }
